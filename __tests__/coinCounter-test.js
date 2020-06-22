20f2dc530bf12150d4e0cfb70d968758facdd162
Tyler Bates <Fondlytyler@gmail.com>
2020-06-22 10:36:07 -0700

    successfully fail/pass second test.

--- __tests__/coinCounter-test.js
+++ __tests__/coinCounter-test.js
@@ -6,4 +6,10 @@ describe ("CoinCounter", ()=> {
     const input = 4.99;
     expect(typeof(input)).toEqual("number");
   })
+
+  test('should verify if input is not blank', () => {
+    const input = parseInt("");
+    expect(input).toEqual(NaN);
+  })
+
 })
